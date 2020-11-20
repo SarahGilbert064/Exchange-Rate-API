d5029be8f86a978569a15b1299bef9e3aa202540
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-20 11:28:41 -0800

    correctly fetch API and show in console log, had lots of issues with npm start but I think i figured it out

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
+import Exchange from '../services/exchangeService.js';
+
+
+function displayRate(response) {
+  if (response.conversion_rates) {  
+    $('#rates').append(`${response.coversion_rates}`);
+    }
+  }
+
+
+
 
 $(document).ready(function() {
   $('#final-amount').click(function() {
-    const usd = $('#dollar-amount').val();
-
-
-
-    let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD`;
-
-    request.onload = function() {
-      if (this.status === 200 {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-
-      }
-    )}
-  })
+    Exchange.getExchange()
+      .then(function(response) {
+        displayRate(response);
+        console.log(response);
+      })
+  });
 });
