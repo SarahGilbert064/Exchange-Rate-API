0852dfd2b92ad2aa414628f3b33bd7b4a64f345f
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-20 09:58:37 -0800

    add api to main and key to env file

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
+
+$(document).ready(function() {
+  $('#final-amount').click(function() {
+    const usd = $('#dollar-amount').val();
+
+
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD`;
+
+    request.onload = function() {
+      if (this.status === 200 {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+
+      }
+    )}
+  })
+});
