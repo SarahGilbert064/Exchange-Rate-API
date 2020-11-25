5d649fb04cdcf6502be71f30fa7efdd29ae7e3c1
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-25 12:29:02 -0800

    change exchangeService into static method with promise and fetch api

--- src/services/exchangeService.js
+++ src/services/exchangeService.js
@@ -1,18 +1,17 @@
+import { parse } from "@babel/core";
+
 export default class ExchangeService {
   static getExchange() {
-    return new Promise (function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const link = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
 
-      request.onload = function() {
-        if(this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-      request.open("GET", link, true);
-      request.send();
-    });
-  }  
-}
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}    
